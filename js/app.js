bb13be63f8b5a9a0c2c7759aad67436dbcfb321c
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-02-23 16:47:39 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -5,15 +5,52 @@ const loadData =async (url) =>{
     try{
         
         const res = await fetch(url);
-        const data = await res.json();displayCountries(data);
+        const data = await res.json(); displayCountries(data);
         }catch(e){
     
         }
 }
+
 const loadCountries =  () =>{
     const url ='https://restcountries.com/v3.1/all';
 
     loadData(url);
+}
+
+const loadDeatil= async (countryName)=>{
+    const url =`https://restcountries.com/v3.1/name/${countryName}`;
+    
+    try{
+        
+        const res = await fetch(url);
+        const data = await res.json(); showDetail(data[0]);
+        }catch(e){
+    
+        }
+
+}
+
+const showDetail = (country) =>{
+    const showDetail = document.getElementById('show-detail');
+    console.log(country);
+    showDetail.textContent='';
+
+    showDetail.innerHTML = `
+    <img src="${country.flags.png}" class="card-img-top" alt="${country.name.common} Flag">
+        <div class="card-body">
+          <h5 class="card-title">${country.name.common}</h5>
+         <div class="d-flex justify-content-between">
+         <p class="card-text">Capital: ${country.capital}
+         <p class="card-text">Region: ${country.region}
+         <p class="card-text">Population: ${country.population}
+         </p>
+         </div>
+         <div class="d-flex justify-content-between">
+         <p class="card-text">Independent: ${country.independent}
+         
+         </div>
+        </div>
+    `
 }
 
 loadCountries();
@@ -26,7 +63,7 @@ const displayCountries = (countries) =>{
         div.classList.add('col');
         
         div.innerHTML = `
-        <div class="card h-100">
+        <div onclick="loadDeatil('${country.name.common}')" class="card h-100">
                 <img height="150px" src="${country.flags.png}" class="card-img-top" alt="${country.name.common} Flag">
                 <div class="card-body">
                   <h5 class="card-title text-center">${country.name.common}</h5>
@@ -44,7 +81,6 @@ const searchCountry = ()=> {
     const countryName = searchInput.value;
     const url =`https://restcountries.com/v3.1/name/${countryName}`;
     
-    console.log(url)
     loadData(url);
 
 }
